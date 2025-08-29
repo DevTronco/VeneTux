5473859f60f28f1f35ced9b84ec82bf4f39f1b91
Tronco <enricocerato10@gmail.com>
2025-08-29 22:57:56 +0200

    just fixed order

--- headers/string.h
+++ headers/string.h
@@ -1,9 +1,18 @@
 #include "utils.h"
 
 typedef const char* u_string;
+typedef char* string;
 
-struct{
-    const char string;
-    size_t len;
-} String;
+void putstr(u_string sentence){
+    volatile unsigned char* vidmem = (volatile unsigned char*) 0xb8000;
+    while(*sentence){
+        vidmem[pos++] = *sentence++;
+        vidmem[pos++] = 0x07;
+    }
+}
 
+void more_args_str(u_string strings[], int count){
+    for (int i = 0; i < count; i++){
+        putstr(strings[i]);
+    }
+}
--- kernel.c
+++ kernel.c
@@ -15,20 +15,6 @@ void putchar(char c){
     vidmem[pos++] = 0x07;
 }
 
-void putstr(u_string sentence){
-    volatile unsigned char* vidmem = (volatile unsigned char*) 0xb8000;
-    while(*sentence){
-        vidmem[pos++] = *sentence++;
-        vidmem[pos++] = 0x07;
-    }
-}
-
-void more_args_str(u_string strings[], int count){
-    for (int i = 0; i < count; i++){
-        putstr(strings[i]);
-    }
-}
-
 void k_clear_screen(int bg) {
     volatile char* video = (volatile char*)0xB8000; //video memory
     for (int i = 0; i < 80 * 25 * 2; i += 2) {
