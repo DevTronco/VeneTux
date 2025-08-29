ef7ea52318bb231d2e088f709006ff20bd97a9a6
Tronco <enricocerato10@gmail.com>
2025-08-29 22:50:55 +0200

    fixed strings and boot (it wasnt grub)

--- kernel.c
+++ kernel.c
@@ -1,19 +1,31 @@
 #include "headers/utils.h"
 #include "headers/string.h"
 
+int pos = 0;
+
+void newline(){
+    int row = pos / (80 * 2);
+    row++;
+    pos = row * 80 * 2;
+}
+
 void putchar(char c){
     volatile char* vidmem = (volatile char*) 0xb8000;
-    static int pos = 0;
     vidmem[pos++] = c;
     vidmem[pos++] = 0x07;
 }
 
-void putstr(u_string *sentence){
+void putstr(u_string sentence){
     volatile unsigned char* vidmem = (volatile unsigned char*) 0xb8000;
-    static int pos;
     while(*sentence){
-        vidmem[pos++] = sentence++;
+        vidmem[pos++] = *sentence++;
         vidmem[pos++] = 0x07;
+    }
+}
+
+void more_args_str(u_string strings[], int count){
+    for (int i = 0; i < count; i++){
+        putstr(strings[i]);
     }
 }
 
@@ -26,6 +38,11 @@ void k_clear_screen(int bg) {
 }
 
 void kmain(){
+    u_string nomi[] = {"Adolf ", "Hitler"};
+    k_clear_screen(0x0);
     putchar('A');
-    putstr("test");
+    newline();
+    putstr("sono un sigma");
+    newline();
+    more_args_str(nomi, 2);
 }
