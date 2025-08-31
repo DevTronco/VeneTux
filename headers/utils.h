1732adb17b2a4c377911c527c00c513775a26004
Tronco <enricocerato10@gmail.com>
2025-08-31 22:28:10 +0200

    restarting from (almost) 0

--- headers/utils.h
+++ headers/utils.h
@@ -1,8 +1,20 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+
+volatile char* vidmem = (volatile char*) 0xb8000;
+
+unsigned int rm_bg(int x){
+    for (int i = 0; i < VGA_HEIGHT * VGA_WIDTH * 2; i+=2){
+        vidmem[i++] = ' ';
+        vidmem[i++] = 0x0f;
+    }
+}
+
+
 unsigned char putchar(char c){
-    volatile char* vidmem = (volatile char*) 0xb8000;
     static int pos = 0;
     vidmem[pos++] = c;
     vidmem[pos++] = 0x0f; //white
