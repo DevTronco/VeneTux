80641d32ae7a2b15a527a55bff799b694304a5d8
Tronco <enricocerato10@gmail.com>
2025-08-31 22:40:28 +0200

    just remade a few things to print

--- headers/utils.h
+++ headers/utils.h
@@ -5,17 +5,17 @@
 #define VGA_HEIGHT 25
 
 volatile char* vidmem = (volatile char*) 0xb8000;
+static int pos = 0;
 
 unsigned int rm_bg(int x){
     for (int i = 0; i < VGA_HEIGHT * VGA_WIDTH * 2; i+=2){
-        vidmem[i++] = ' ';
-        vidmem[i++] = 0x0f;
+        vidmem[i] = ' ';
+        vidmem[i+1] = 0x0f;
     }
 }
 
 
 unsigned char putchar(char c){
-    static int pos = 0;
     vidmem[pos++] = c;
     vidmem[pos++] = 0x0f; //white
 }
--- kernel.c
+++ kernel.c
@@ -1,6 +1,8 @@
 #include "headers/utils.h"
-
+#include "headers/string.h"
 
 void kmain(){
+    rm_bg(0x0);
     putchar('W');
+    putstr(" ciao mondo");
 }
