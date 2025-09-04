22bc38fb1da7c48f231ae6859ddbcbb39433c336
Tronco <enricocerato10@gmail.com>
2025-09-04 22:29:07 +0200

    remade types

--- headers/utils.h
+++ headers/utils.h
@@ -4,6 +4,23 @@
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
 
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+typedef signed long long int64_t;
+
+#if ARCH_64BIT 
+typedef uint64_t size_t;
+#else
+typedef uint32_t size_t;
+#endif
+
 volatile char* vidmem = (volatile char*) 0xb8000;
 static int pos = 0;
 
